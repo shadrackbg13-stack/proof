fac40f9bc9195ddfd6110da6e3ed6b90ab38bf9a
shadrackbg13-stack <shadrackbg13@gmail.com>
2026-06-10 00:39:11 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+player.onChat("run", function () {
+	
+})
+gameplay.setWeather(THUNDER)
+mobs.spawn(CHICKEN, pos(0, 0, 0))
+player.say("hi")
+blocks.place(GRASS, pos(0, 0, 0))
+blocks.place(GRASS, pos(1, 1, 1))
